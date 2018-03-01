11fa3c5f4fcd4f06458e8d7f74f1fa882691d08d
Philippe Moneda <philippemoneda@gmail.com>
2018-03-01 10:44:50 -0300

    atualiza versao domain app

--- plataforma_apicore/model/tests/test_domain.py
+++ plataforma_apicore/model/tests/test_domain.py
@@ -1,13 +1,6 @@
-from model.domain import MigrationHistory, conta
-from migration.sync import sync_db
+from model.domain import conta
+from migration.sync import sync_db, drop_database
 from database import create_session
-
-
-def test_save_migration_history(session):
-    m = MigrationHistory(name="teste")
-
-    session.add(m)
-    session.commit()
 
 def test_save_conta():
     sync_db()
@@ -15,4 +8,4 @@ def test_save_conta():
     session = create_session()
     session.add(c)
     session.commit()
-    assert session.query(Conta).count() == 1
+    assert session.query(conta).count() >= 1
