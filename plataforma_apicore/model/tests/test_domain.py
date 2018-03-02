96c7e4b79a35be2d97ecb879b45d33ca0b2f5060
Philippe Moneda <philippemoneda@gmail.com>
2018-03-02 10:30:16 -0300

    atualiza versao

--- plataforma_apicore/model/tests/test_domain.py
+++ plataforma_apicore/model/tests/test_domain.py
@@ -2,10 +2,8 @@ from model.domain import conta
 from migration.sync import sync_db
 from database import create_session
 
-def test_save_conta():
-    sync_db()
+def test_save_conta(session):
     c = conta(saldo=1, titular="Teste")
-    session = create_session()
     session.add(c)
     session.commit()
     assert session.query(conta).count() >= 1
