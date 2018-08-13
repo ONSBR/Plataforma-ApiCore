b12f11213e33edbb32219e2bf0d5d2b6a6e6247d
Philippe Moneda <philippemoneda@gmail.com>
2018-08-13 12:01:26 -0300

    ajustes

--- plataforma_apicore/main.py
+++ plataforma_apicore/main.py
@@ -1,7 +1,9 @@
 ''' Main Module starts all domain app components '''
 from migration.sync import wait_postgres
 from database import create_db
+import log
 
+log.info("apicore: 0.0.1")
 
 if wait_postgres():
     create_db()
